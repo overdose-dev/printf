76f8225b7dfa475f18bc0c62c81217c95904b7da
dev-olanma <olanmaorji13@gmail.com>
2023-03-28 06:52:54 -0700

    Modify handle_print.c file

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
@@ -12,14 +11,11 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
-
 	fmt_t fmt_types[] = {
-
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
@@ -28,24 +24,21 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-		return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
-				--(*ind);
+			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-
 			if (fmt[*ind] == ' ')
-
 				--(*ind);
 			return (1);
 		}
