f253db7fc5611b746b50e06ad389558e15042382
overdose-dev <chinazauchenna@gmail.com>
2023-03-28 07:43:24 -0700

    arranged indentation

--- main.h
+++ main.h
@@ -24,6 +24,7 @@
  * @fmt: The format.
  * @fn: The function associated.
  */
+
 struct fmt
 {
 	char fmt;
@@ -36,6 +37,7 @@ struct fmt
  * @fmt: The format.
  * @fm_t: The function associated.
  */
+
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
